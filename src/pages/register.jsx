52d7c742f8bf64494cd786ac5464832f576e5ed5
BishalRaj <bisal1shakya@gmail.com>
2022-10-30 18:29:53 +0000

    Created authentication page using material ui

---
+++ src/pages/register.jsx
@@ -0,0 +1,117 @@
+import {
+  Button,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { MdVisibility, MdVisibilityOff } from "react-icons/md";
+import AuthLayout from "../layout/authLayout";
+import { color, image } from "../static";
+const Register = () => {
+  const formWidth = {
+    width: "50%",
+    "@media (max-width: 426px)": {
+      width: "80%",
+    },
+  };
+  const { register, handleSubmit } = useForm();
+  const handleRegister = (data) => {
+    alert(`email: ${data.email}  pwd: ${data.password} `);
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  const formComponent = (
+    <Stack
+      spacing={2}
+      sx={formWidth}
+      component="form"
+      onSubmit={handleSubmit(handleRegister)}
+    >
+      <Typography
+        variant="h4"
+        component="div"
+        color={color.default}
+        sx={{ fontWeight: 700 }}
+      >
+        Sign up
+      </Typography>
+
+      <TextField
+        id="outlined-basic"
+        label="Full Name"
+        variant="outlined"
+        type="text"
+        sx={{ width: "100%" }}
+        {...register("name", { required: true })}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Email"
+        variant="outlined"
+        type="email"
+        sx={{ width: "100%" }}
+        {...register("email", { required: true })}
+      />
+
+      <FormControl sx={{ width: "100%" }} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={showPassword ? "text" : "password"}
+          {...register("password", { required: true })}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <MdVisibility /> : <MdVisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </FormControl>
+
+      <Button
+        variant="contained"
+        sx={{ textTransform: "none", backgroundColor: color.default }}
+        className="py-2"
+        type="submit"
+      >
+        <Typography>Register</Typography>
+      </Button>
+    </Stack>
+  );
+
+  const redirectComponent = {
+    text: "Already an account?",
+    redirectText: "Login",
+    link: "/",
+  };
+  return (
+    <AuthLayout
+      formComponent={formComponent}
+      redirectComponent={redirectComponent}
+      authCardText={"Hi, Welcome back"}
+      authCardImg={image.register}
+    />
+  );
+};
+
+export default Register;
---
+++ src/static/index.js
@@ -0,0 +1,12 @@
+export const color = {
+  default: "#00ab55",
+  hoverDefault: "#009248",
+};
+
+export const image = {
+  logo: "https://i.ibb.co/2PwXW2v/35257006-removebg-preview.png",
+  login:
+    "https://image.freepik.com/free-vector/account-concept-illustration_114360-399.jpg",
+  register:
+    "https://image.freepik.com/free-vector/mobile-login-concept-illustration_114360-135.jpg",
+};
